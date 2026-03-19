a0553d5b3b548cc8beaace9574d02930947ebf20
Pauline-hiez <pauline.hiez@laplateforme.io>
2026-03-19 14:20:42 +0100

    HOME PAGE - Création des fichier + Remplissage de homeTypes

---
+++ frontend/src/view/home/homeTypes.ts
@@ -0,0 +1,17 @@
+export interface StatItem {
+    value: string;
+    label: string;
+}
+
+export interface TimeLeft {
+    jours: number;
+    heures: number;
+    min: number;
+    sec: number;
+}
+
+export interface PanelRow {
+    label: string;
+    value: string;
+}
+
