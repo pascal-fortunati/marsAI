f44238e78e26f0202d913a63fe75527e56fb24f5
Pauline-hiez <pauline.hiez@laplateforme.io>
2026-03-27 13:26:00 +0100

    nettoyage

--- backend/src/routes/home.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getHome } from "../controllers/homeController.js";
-
-const router = Router();
-// GET /api/home - Récupérer les données pour la page d'accueil
-router.get("/", getHome);
-
-export default router;
--- backend/src/services/submissionService.js
+++
@@ -1,6 +0,0 @@
-import { insertSubmission } from "../models/submissionModel.js";
-
-export async function createSubmission(payload) {
-    const id = await insertSubmission(payload);
-    return { id, email: payload.director_email };
-}
