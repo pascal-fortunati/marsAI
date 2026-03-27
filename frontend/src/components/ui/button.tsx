af6c614e8e3c0edc5877b030fe34a40701075d60
sylvain-malbon <sylvain.malbon@laplateforme.io>
2026-03-27 15:15:43 +0100

    add buton ui

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -1,18 +1,24 @@
-import * as React from 'react'
-import { Slot } from '@radix-ui/react-slot'
-import type { VariantProps } from 'class-variance-authority'
-import { cn } from '../../lib/utils'
-import { buttonVariants } from './button-variants'
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import type { VariantProps } from "class-variance-authority";
+import { cn } from "../../lib/utils";
+import { buttonVariants } from "./button-variants";
 
 export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
-  }
+    asChild?: boolean;
+  };
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button'
-    return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
   },
-)
-Button.displayName = 'Button'
+);
+Button.displayName = "Button";
